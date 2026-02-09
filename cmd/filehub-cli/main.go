3f13fb3e28af5049a9fce226284ff6287b321851
zqr233 <zqr@zqrs-macbook.local>
2026-02-09 09:16:44 +0800

    fix: stop ignoring cmd packages

---
+++ cmd/filehub-cli/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/kiry163/filehub/internal/cli"
+
+func main() {
+	cli.Execute()
+}
---
+++ cmd/filehub/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+
+	"github.com/kiry163/filehub/internal/api"
+	"github.com/kiry163/filehub/internal/config"
+	"github.com/kiry163/filehub/internal/db"
+	"github.com/kiry163/filehub/internal/service"
+	"github.com/kiry163/filehub/internal/storage"
+)
+
+func main() {
+	cfg, err := config.Load("config.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := os.MkdirAll(filepath.Dir(cfg.Database.Path), 0o755); err != nil {
+		log.Fatal(err)
+	}
+
+	database, err := db.Open(cfg.Database.Path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	minioStorage, err := storage.NewMinioStorage(context.Background(), cfg.Minio)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	svc := &service.Service{
+		DB:      database,
+		Storage: minioStorage,
+		Config:  cfg,
+	}
+
+	router := api.NewRouter(svc)
+	address := ":" + strconv.Itoa(cfg.Server.Port)
+	log.Printf("filehub server listening on %s", address)
+	if err := http.ListenAndServe(address, router); err != nil {
+		log.Fatal(err)
+	}
+}
